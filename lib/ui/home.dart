c0bfab5b7e01d558b48d6ad374447a72080fa047
naamini <carollacharles@gmail.com>
2018-04-17 16:19:28 +0300

    icons on the appBar

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget{
+
+  void _onpressed(){
+    print("searching....");
+  }
+
+
   @override
   Widget build(BuildContext context) {
 
-    new Scaffold(
+    return new Scaffold(
       appBar: new AppBar(
         backgroundColor: Colors.amber,
         title: new Text("Fency day"),
 
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.home),
+              onPressed: () => debugPrint('working!!')),
+
+          new IconButton(icon: new Icon(Icons.search),
+              onPressed: _onpressed),
+
+        ],
       ),
     );
   }
