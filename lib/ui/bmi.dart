a7939251deadc5ecb5fb6bcdefdf608788865a79
naamini <carollacharles@gmail.com>
2018-04-19 09:30:19 +0300

    created my own BMI app

--- lib/ui/bmi.dart
+++ lib/ui/bmi.dart
@@ -8,6 +8,48 @@ class BmiApp extends StatefulWidget {
 }
 
 class BmiChallenge extends State<BmiApp> {
+
+  final TextEditingController _ageController = new TextEditingController();
+  final TextEditingController _heightController = new TextEditingController();
+  final TextEditingController _weightController = new TextEditingController();
+  double _finalBMINo = 0.0;
+  String _finalBMI = "";
+  String status = "";
+
+
+  void handleCal(){
+    setState(() {
+      _finalBMINo = calculateBMI(_weightController.text,_heightController.text,703.0);
+      if(_finalBMINo < 5){
+       status = "Underweight";
+      }else{
+        status = "OverWeight";
+      }
+      _finalBMI = "Your BMI ${_finalBMINo.toStringAsFixed(1)}";
+      });
+  }
+
+
+  double calculateBMI(String weight, String height, double multiplier) {
+    if(int.parse(weight).toString().isNotEmpty && int.parse(weight) >0){
+      if(int.parse(height).toString().isNotEmpty && int.parse(height) >0){
+       int doubleHeightFinal = (int.parse(height) * int.parse(height));
+        print(doubleHeightFinal);
+       double dividing =  int.parse(weight) / doubleHeightFinal;
+        print(dividing);
+        print(dividing*multiplier);
+        return dividing * multiplier;
+      }
+    }else{
+      print("EMPTY");
+    }
+    //return default planet
+    return int.parse("180")*703.0;
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -32,6 +74,7 @@ class BmiChallenge extends State<BmiApp> {
                 child: new Column(
                   children: <Widget>[
                     new TextField(
+                      controller: _ageController,
                       keyboardType: TextInputType.number,
                       decoration: new InputDecoration(
                         labelText: "Age",
@@ -40,6 +83,7 @@ class BmiChallenge extends State<BmiApp> {
                       ),
                     ),
                     new TextField(
+                      controller: _heightController,
                       keyboardType: TextInputType.number,
                       decoration: new InputDecoration(
                         labelText: "Height in feet",
@@ -48,6 +92,7 @@ class BmiChallenge extends State<BmiApp> {
                       ),
                     ),
                     new TextField(
+                      controller: _weightController,
                       keyboardType: TextInputType.number,
                       decoration: new InputDecoration(
                         labelText: "Weight in lb",
@@ -57,7 +102,7 @@ class BmiChallenge extends State<BmiApp> {
                     ),
                     new Padding(padding: new EdgeInsets.all(10.5)),
                     new RaisedButton(
-                      onPressed: () => debugPrint("pressing"),
+                      onPressed: handleCal,
                       color: Colors.pinkAccent.shade200,
                       child: new Text(
                         "Calculate",
@@ -67,16 +112,36 @@ class BmiChallenge extends State<BmiApp> {
                         ),
                       ),
                     ),
-
-                    new Padding(padding: EdgeInsets.all(10.5)),
-
+                    new Padding(padding: new EdgeInsets.all(5.5)),
 
                   ],
                 ),
-              )
+              ),
+              new Padding(padding: EdgeInsets.all(5.5)),
 
-
-
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Column(
+                    children: <Widget>[
+                      new Text(
+                        _finalBMI,
+                        style: new TextStyle(
+                            color: Colors.blueAccent,
+                            fontSize: 25.5,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      new Text(
+                        status,
+                        style: new TextStyle(
+                            color: Colors.pink.shade200,
+                            fontSize: 29.5,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ],
           ),
         ));
